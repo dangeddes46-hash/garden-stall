28a90760254fa65c45f79519325214391b9925bf
dangeddes46-hash <dangeddes46@gmail.com>
2026-07-02 22:01:39 +0100

    Update version to 0.1.20

--- prototype/src/state/initialState.js
+++ prototype/src/state/initialState.js
@@ -2,7 +2,7 @@ import { PHASES, STARTING_CASH } from '../data/constants.js';
 import { createTradingClock } from '../systems/tradingClockSystem.js';
 
 export const initialState = {
-  prototypeVersion: '0.1.19-live-timer-skeleton',
+  prototypeVersion: '0.1.20-evening-order-feedback',
   phase: PHASES.DAY0_ORDER,
   currentDay: 0,
   cash: STARTING_CASH,
