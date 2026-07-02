ace8d6b8ecfa0c1c57aea07b101cb01509128c5e
dangeddes46-hash <dangeddes46@gmail.com>
2026-07-02 03:14:46 +0100

    Bump prototype to 0.1.10 clarity tuning

--- prototype/src/state/initialState.js
+++ prototype/src/state/initialState.js
@@ -1,7 +1,7 @@
 import { PHASES, STARTING_CASH } from '../data/constants.js';
 
 export const initialState = {
-  prototypeVersion: '0.1.9-notebook-feedback',
+  prototypeVersion: '0.1.10-clarity-tuning',
   phase: PHASES.DAY0_ORDER,
   currentDay: 0,
   cash: STARTING_CASH,
