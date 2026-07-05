30572a55918aae537fcc7270d1b921c31d778dc3
dangeddes46-hash <dangeddes46@gmail.com>
2026-07-05 10:58:23 +0100

    Bump version to 0.1.25.1

--- prototype/src/state/initialState.js
+++ prototype/src/state/initialState.js
@@ -3,7 +3,7 @@ import { createTradingClock } from '../systems/tradingClockSystem.js';
 import { createInitialWeekStats } from '../systems/weekStatsSystem.js';
 
 export const initialState = {
-  prototypeVersion: '0.1.25-stock-handling-summary-readability',
+  prototypeVersion: '0.1.25.1-transport-unit-correction',
   phase: PHASES.DAY0_ORDER,
   currentDay: 0,
   cash: STARTING_CASH,
