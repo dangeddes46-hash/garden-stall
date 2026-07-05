cb26d79022bd979df58a510c626b46e175db91b0
dangeddes46-hash <dangeddes46@gmail.com>
2026-07-05 08:53:54 +0100

    Add sundry and grouped potted van capacity

--- prototype/src/data/vanCapacity.js
+++ prototype/src/data/vanCapacity.js
@@ -1,6 +1,16 @@
 export const VAN_LOAD_LIMITS = {
   traySlots: 6,
-  featurePots: 6
+  featurePots: 6,
+  sundrySlots: 3
 };
 
 export const FEATURE_POT_TRAY_FORMATS = ['feature-pot-group'];
+
+export const FEATURE_POT_UNITS_PER_SLOT = {
+  'plant-potted-lily': 3
+};
+
+export const SUNDRY_UNITS_PER_SLOT = {
+  'sundry-compost': 1,
+  'sundry-terracotta-pot': 10
+};
